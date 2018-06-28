f4ef4ba6e9f18fbc26135915df2975929ef9e1a5
Jason Prendergast <j1mbob200@gmail.com>
2018-06-28 12:31:32 +0100

    Fix: add exceptions

--- produceoutput.py
+++ produceoutput.py
@@ -3,12 +3,15 @@ import handleinputsdb
 
 class ProduceOutput:
     def process_input_data(self, data_path, inc_db, inc_response):
-        # create handleinputdbobj
-        HandleInputDbobj = handleinputsdb.HandleInputDb()
-        # Create the handle inputs object
-        HandleInputs = handleinputs.HandleInputs()
-        # Run the handle inputs method read all
-        data_frame_dict = HandleInputs.read_all_files(data_path, inc_db, inc_response, HandleInputDbobj)
-        # Run merge all the dataframes in dict to a single dataframe
-        csv_merged = HandleInputs.merge_dataframe_list(data_frame_dict)
-        return csv_merged
+        try:
+            # create handleinputdbobj
+            HandleInputDbobj = handleinputsdb.HandleInputDb()
+            # Create the handle inputs object
+            HandleInputs = handleinputs.HandleInputs()
+            # Run the handle inputs method read all
+            data_frame_dict = HandleInputs.read_all_files(data_path, inc_db, inc_response, HandleInputDbobj)
+            # Run merge all the dataframes in dict to a single dataframe
+            csv_merged = HandleInputs.merge_dataframe_list(data_frame_dict)
+            return csv_merged
+        except Exception as ex:
+            print(ex)
